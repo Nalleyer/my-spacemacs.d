86b9ac23cd8c605bbe9c089ab1e259aa9d891b63
nalleyer <nalleyer@gmail.com>
2018-08-31 09:20:10 +0800

    add path and vue-mode

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;
+﻿;;
 ;;
 ;- * -mode:emacs - lisp - *-;
 ;;
@@ -37,6 +37,7 @@ values."
      autohotkey
      javascript
      html
+     lua
      ;;(c-c++ :variables
      ;;       c-c++-enable-clang-support t
      ;;       c-c++-default-mode-for-headers 'c++-mode
@@ -63,15 +64,17 @@ values."
               haskell-completion-backend 'intero
               haskell-enable-hindent-style "johan-tibell")
      yaml
+     ;;
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
    dotspacemacs-additional-packages '(
+                                      csharp-mode
                                       perl6-mode
                                       nyan-mode
-                                      shakespeare-mode
+                                      vue-mode
                                       )
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
@@ -298,7 +301,7 @@ values."
    ;; List of search tool executable names. Spacemacs uses the first installed
    ;; tool of the list. Supported tools are `ag', `pt', `ack' and `grep'.
    ;; (default '("ag" "pt" "ack" "grep"))
-   dotspacemacs-search-tools '("ag" "pt" "ack" "grep")
+   dotspacemacs-search-tools '("grep" "ag" "pt" "ack")
    ;; The default package repository used if no explicit repository has been
    ;; specified with an installed package.
    ;; Not used for now. (default nil)
@@ -318,16 +321,23 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+
+    ;; proxy
+    ;(setq url-proxy-services '(
+    ;                          ("http" . "a.b.com:8080")
+    ;                          ("https" . "a.b.com:8080")
+    ;                          ))
+
     ;; elpa china
-    ;(setq configuration-layer--elpa-archives
-    ;  '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
-    ;    ("org-cn"   . "http://elpa.emacs-china.org/org/")
-    ;    ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
-    ;; elpa tsinghua
     (setq configuration-layer--elpa-archives
-    '(("melpa-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-      ("org-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
-      ("gnu-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
+      '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
+        ("org-cn"   . "http://elpa.emacs-china.org/org/")
+        ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
+    ;; elpa tsinghua
+    ;(setq configuration-layer--elpa-archives
+    ;'(("melpa-cn" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+    ;  ("org-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+    ;  ("gnu-cn"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")))
     (setq tramp-ssh-controlmaster-options
           "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
   )
@@ -351,6 +361,9 @@ you should place your code here."
   (nyan-mode)
   (nyan-start-animation)
 
+  ; csharp-mode
+  (add-to-list 'auto-mode-alist '("\\.cs\\'" . csharp-mode))
+
   ;; evil tab key
   (define-key evil-insert-state-map (kbd "TAB") 'tab-to-tab-stop)
 
@@ -372,9 +385,26 @@ you should place your code here."
   ;; haskell indent fix
   ;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
-  ;; shakespeare
-  ;;(shakespeare-mode)
+  ;; lua indentation
+  (with-eval-after-load 'lua-mode
+    (setq lua-indent-level 2))
+  (with-eval-after-load 'org (setq org-agenda-files
+                                   '("D:/todos")))
+  ;; paths
+  (when (string-equal system-type "windows-nt")
+    (let (
+          (mypaths
+           '(
+             ;;"c:/Program Files (x86)/GnuWin32/bin"
+             ;;
+             ) )
+          )
+
+      (setenv "PATH" (concat (mapconcat 'identity mypaths ";") ";" (getenv "path")))
+      (setq exec-path (append exec-path (append mypaths (list "." exec-directory))) )
+      ) )
   )
+
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
@@ -383,9 +413,12 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(helm-ff-fuzzy-matching t)
+ '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (ahk-mode shakespeare-mode yaml-mode intero hlint-refactor hindent helm-hoogle haskell-snippets flycheck-haskell company-ghci company-ghc ghc haskell-mode company-cabal cmm-mode helm-company company-web web-completion-data company-tern dash-functional tern company-statistics company-c-headers ac-ispell helm-c-yasnippet fuzzy company auto-yasnippet auto-complete disaster cmake-mode clang-format web-beautify livid-mode skewer-mode simple-httpd json-mode json-snatcher json-reformat js2-refactor yasnippet multiple-cursors js2-mode js-doc coffee-mode web-mode tagedit slim-mode scss-mode sass-mode pug-mode less-css-mode helm-css-scss haml-mode emmet-mode rainbow-mode rainbow-identifiers color-identifiers-mode nyan-mode perl6-mode unfill smeargle orgit org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-mime org-download mwim mmm-mode markdown-toc markdown-mode magit-gitflow htmlize helm-gitignore gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md flycheck-pos-tip pos-tip flycheck evil-magit magit magit-popup git-commit ghub let-alist with-editor ws-butler winum volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint indent-guide hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg eval-sexp-fu highlight elisp-slime-nav dumb-jump f dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed ace-link ace-jump-helm-line helm helm-core popup which-key undo-tree org-plus-contrib hydra async aggressive-indent adaptive-wrap ace-window avy)))
+    (csharp-mode lua-mode monokai-theme ahk-mode yaml-mode intero hlint-refactor hindent helm-hoogle haskell-snippets flycheck-haskell company-ghci company-ghc ghc haskell-mode company-cabal cmm-mode helm-company company-web web-completion-data company-tern dash-functional tern company-statistics company-c-headers ac-ispell helm-c-yasnippet fuzzy company auto-yasnippet auto-complete disaster cmake-mode clang-format web-beautify livid-mode skewer-mode simple-httpd json-mode json-snatcher json-reformat js2-refactor yasnippet multiple-cursors js2-mode js-doc coffee-mode web-mode tagedit slim-mode scss-mode sass-mode pug-mode less-css-mode helm-css-scss haml-mode emmet-mode rainbow-mode rainbow-identifiers color-identifiers-mode nyan-mode perl6-mode unfill smeargle orgit org-projectile org-category-capture org-present org-pomodoro alert log4e gntp org-mime org-download mwim mmm-mode markdown-toc markdown-mode magit-gitflow htmlize helm-gitignore gnuplot gitignore-mode gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link gh-md flycheck-pos-tip pos-tip flycheck evil-magit magit magit-popup git-commit ghub let-alist with-editor ws-butler winum volatile-highlights vi-tilde-fringe uuidgen use-package toc-org spaceline powerline restart-emacs request rainbow-delimiters popwin persp-mode pcre2el paradox spinner org-bullets open-junk-file neotree move-text macrostep lorem-ipsum linum-relative link-hint indent-guide hungry-delete hl-todo highlight-parentheses highlight-numbers parent-mode highlight-indentation helm-themes helm-swoop helm-projectile helm-mode-manager helm-make projectile pkg-info epl helm-flx helm-descbinds helm-ag google-translate golden-ratio flx-ido flx fill-column-indicator fancy-battery eyebrowse expand-region exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-surround evil-search-highlight-persist evil-numbers evil-nerd-commenter evil-mc evil-matchit evil-lisp-state smartparens evil-indent-plus evil-iedit-state iedit evil-exchange evil-escape evil-ediff evil-args evil-anzu anzu evil goto-chg eval-sexp-fu highlight elisp-slime-nav dumb-jump f dash s diminish define-word column-enforce-mode clean-aindent-mode bind-map bind-key auto-highlight-symbol auto-compile packed ace-link ace-jump-helm-line helm helm-core popup which-key undo-tree org-plus-contrib hydra async aggressive-indent adaptive-wrap ace-window avy)))
+ '(projectile-enable-caching t)
  '(safe-local-variable-values
    (quote
     ((haskell-process-use-ghci . t)
